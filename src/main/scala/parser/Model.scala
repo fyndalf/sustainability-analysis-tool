cf13de3b9322a3c573a51dde824ad28651f3b06c
Finn K <fyndalf@users.noreply.github.com>
2022-04-25 21:26:03 +0200

    Add note for model parsing and manipulation

--- src/main/scala/parser/Model.scala
+++ src/main/scala/parser/Model.scala
@@ -2,4 +2,8 @@ package parser
 
 object Model {
 
+  // determine colour difference based on: max difference between differences, or on percentual increase
+
+  // for each activity: find <bpmn:task> and note id
+  // find bpmnd:BPMNShape with id and set color:background-color="#ffffff" color:border-color="#FF6600"
 }
